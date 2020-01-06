94c696ecf6f81960ad08f70a177cbbc5f849a563
michael-ugborioko-cko <michael.ugborikoko@checkout.com>
2020-01-06 12:30:43 +0000

    New cartridge

---
+++ int_checkoutcom/cartridge/scripts/init/CkoVerifyChargesInit.js
@@ -0,0 +1,50 @@
+/* API Includes */
+var svc = require('dw/svc');
+
+/* Utility */
+var util = require('~/cartridge/scripts/utility/util');
+
+
+/**
+ * Initialize HTTP service for the Checkout.com sandbox charges verification.
+ */
+svc.ServiceRegistry.configure("cko.verify.charges.sandbox.service", {
+    createRequest: function(svc, args) {
+        var serviceUrl = svc.configuration.credential.URL + "/" + args.paymentToken;
+            
+        // Prepare the http service
+        svc.setURL(serviceUrl);
+        svc.setRequestMethod("GET");
+        svc.addHeader("Authorization", util.getAccountKeys().secreteKey);
+        svc.addHeader("User-Agent", util.getCartridgeMeta());
+        svc.addHeader("Content-Type", 'application/json;charset=UTF-8');    
+        
+        return (args) ? JSON.stringify(args) : null;
+	},
+
+    parseResponse: function(svc, resp) {    	
+        return JSON.parse(resp.text);
+    }
+});
+
+/**
+ * Initialize HTTP service for the Checkout.com sandbox charges verification.
+ */
+svc.ServiceRegistry.configure("cko.verify.charges.live.service", {
+    createRequest: function(svc, args) {
+        var serviceUrl = svc.configuration.credential.URL + "/" + args.paymentToken;
+            
+        // Prepare the http service
+        svc.setURL(serviceUrl);
+        svc.setRequestMethod("GET");
+        svc.addHeader("Authorization", util.getAccountKeys().secreteKey);
+        svc.addHeader("User-Agent", util.getCartridgeMeta());
+        svc.addHeader("Content-Type", 'application/json;charset=UTF-8');
+        
+        return (args) ? JSON.stringify(args) : null;
+	},
+
+    parseResponse: function(svc, resp) {
+        return JSON.parse(resp.text);
+    }
+});
