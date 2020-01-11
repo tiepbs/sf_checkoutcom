9b7fc0176b2a3f8345bbfdcb332e0a4dc5a54253
david-fiaty-cko <david.fiaty@checkout.com>
2020-01-11 02:36:33 +0000

    Files update

---
+++ int_checkoutcom/cartridge/controllers/Klarna 2.js
@@ -0,0 +1,61 @@
+/**
+ * Klarna controller.
+ *
+ * @module controllers/Hello
+ */
+
+'use strict';
+
+/* API Includes */
+var siteControllerName = dw.system.Site.getCurrent().getCustomPreferenceValue('ckoStorefrontController');
+var guard = require(siteControllerName + '/cartridge/scripts/guard');
+var BasketMgr = require('dw/order/BasketMgr');
+
+/** Utility **/
+var ckoUtility = require('~/cartridge/scripts/helpers/ckoUtility');
+
+
+function klarnaSession(){
+	
+	var basket = BasketMgr.getCurrentBasket();
+	
+	if(basket){
+
+		var countryCode = ckoUtility.getAppModeValue('GB', ckoUtility.getBasketCountyCode(basket));
+		var currency = ckoUtility.getAppModeValue('GBP', basket.getCurrencyCode());
+		var locale = ckoUtility.getAppModeValue('en-GB', ckoUtility.getLanguage());
+		var total = ckoUtility.getFormattedPrice(basket.getTotalGrossPrice().value, currency);
+		var tax =  ckoUtility.getFormattedPrice(basket.getTotalTax().value, currency);
+		var products = ckoUtility.getBasketObject(basket);
+		var billing = ckoUtility.getBasketAddress(basket);
+				
+		var requestObject = {
+		    "purchase_country": countryCode,
+		    "currency"				: currency,
+		    "locale"				: locale,
+		    "amount"				: total,
+		    "tax_amount"			: tax,
+		    "products"				: products,
+		    "billing_address"		: billing
+		}
+		
+		
+		// Perform the request to the payment gateway
+		var gSession = ckoUtility.gatewayClientRequest('cko.klarna.session.' + ckoUtility.getValue('ckoMode') + '.service', requestObject);
+		
+		gSession.requestObject = requestObject;
+		gSession.addressInfo = ckoUtility.getBasketAddress(basket);
+
+	   if(gSession){
+		   response.getWriter().println(JSON.stringify(gSession));
+	   }
+		
+	}
+	else{
+		response.getWriter().println('Basket Not Found');
+	}
+	
+}
+
+
+exports.klarnaSession = guard.ensure(['https'], klarnaSession);
---
+++ int_checkoutcom/cartridge/scripts/helpers/CKOEvent 2.js
@@ -0,0 +1,108 @@
+'use strict';
+
+/* API Includes */
+var OrderMgr = require('dw/order/OrderMgr');
+var Transaction = require('dw/system/Transaction');
+
+/* Checkout.com Helper functions */
+var ckoUtility = require('~/cartridge/scripts/helpers/ckoUtility');
+
+/**
+ * Gateway event functions for the Checkout.com cartridge integration.
+ */
+var CKOEvent = {
+    /**
+     * Adds the gateway webhook information to the newly created order.
+     */
+    addWebhookInfo: function (hook, paymentStatus, orderStatus) {
+        // Load the order
+        var order = OrderMgr.getOrder(hook.data.reference);
+        if (order) {
+            // Prepare the webhook info
+            var details = '';
+            details += ckoUtility._('cko.webhook.event', 'cko') + ': ' + hook.type + '\n';
+            details += ckoUtility._('cko.transaction.id', 'cko') + ': ' + hook.data.action_id + '\n';
+            details += ckoUtility._('cko.transaction.paymentId', 'cko') + ': ' + hook.data.id + '\n';
+            details += ckoUtility._('cko.transaction.eventId', 'cko') + ': ' + hook.id + '\n';
+            details += ckoUtility._('cko.response.code', 'cko') + ': ' + hook.data.response_code + '\n';
+
+            // Process the transaction
+            Transaction.wrap(function() {
+                // Add the details to the order
+                order.addNote(ckoUtility._('cko.webhook.info', 'cko'), details);
+    
+                // Update the payment status
+                if (paymentStatus) {
+                    order.setPaymentStatus(order[paymentStatus]);	
+                }
+
+                // Update the order status
+                if ((orderStatus) && (orderStatus.indexOf("CANCELLED") != -1 || orderStatus.indexOf("FAILED") != -1)) {
+                    OrderMgr.failOrder(order);
+                }
+            });
+        }
+    },
+
+    /**
+     * Payment captured event.
+     */
+    paymentCaptured: function(hook) {  
+        this.addWebhookInfo(hook, 'PAYMENT_STATUS_PAID', null);
+    },
+
+    /**
+     * Payment authorized event.
+     */
+    paymentApproved: function(hook) {
+        this.addWebhookInfo(hook, 'PAYMENT_STATUS_NOTPAID', null);
+    },   
+
+    /**
+     * Authorization failed event.
+     */
+    paymentDeclined: function(hook) {
+        this.addWebhookInfo(hook, 'PAYMENT_STATUS_NOTPAID', 'ORDER_STATUS_FAILED');
+    },   
+
+    /**
+     * Capture failed event.
+     */
+    paymentCapturedDeclined: function(hook) {
+        this.addWebhookInfo(hook, 'PAYMENT_STATUS_NOTPAID', 'ORDER_STATUS_FAILED');
+    },  
+
+    /**
+     * Payment refunded event.
+     */
+    paymentRefunded: function(hook) {
+        this.addWebhookInfo(hook, 'PAYMENT_STATUS_PAID', 'ORDER_STATUS_CANCELLED');
+    },  
+
+    /**
+     * Payment voided event.
+     */    
+    paymentVoided: function(hook) {
+        this.addWebhookInfo(hook, 'PAYMENT_STATUS_NOTPAID', 'ORDER_STATUS_CANCELLED');
+    }, 
+
+    /**
+     * Refund failed event.
+     */
+    paymentRefundDeclined: function(hook) {
+        this.addWebhookInfo(hook, null, null);
+    }, 
+
+    /**
+     * Charge void failed event.
+     */
+    paymentVoidDeclined: function(hook) {
+        this.addWebhookInfo(hook, null, null);
+    }  
+};
+
+/*
+ * Module exports
+ */
+
+module.exports = CKOEvent;
