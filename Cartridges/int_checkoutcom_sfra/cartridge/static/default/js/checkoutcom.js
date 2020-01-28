7cddbbcbed374f58f84be9f68271380a488b7084
david-fiaty-cko <david.fiaty@checkout.com>
2020-01-28 11:39:47 +0000

    Updated the payment flow display

--- Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/checkoutcom.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/checkoutcom.js
@@ -4,44 +4,8 @@
  * jQuery Ajax helpers on DOM ready.
  */
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the card lsit url
-    var cardUrl = $('[id="ckoCardListUrl"]').val();
-    
-    // Retrieve the card list
-    getCardsList(cardUrl);
     
 }, false);
-
-/**
- * Get the user cards list.
- */
-function getCardsList(dataUrl)
-{
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', dataUrl);
-    xhr.onload = function () {
-        if (this.status == 200) {
-            // Cleanup the response
-            var cards = JSON.parse(this.response.replace(/&quot;/g,'"'));
-            
-            // Show the card selector if there are cards
-            if (cards.length > 0) {
-                $('[id="ckoCardSelector"]').show();
-            }
-            
-            // Add the cards to the select list
-            for (var i = 0; i < cards.length; i++) {
-                if (cards[i].cardNumber) {
-                    $('[id="ckoCreditCardList"]').append($('<option/>', {
-                        value: cards[i].cardId,
-                        text: cards[i].cardNumber,
-                    }));
-                }
-            }
-        }
-    };
-    xhr.send();
-}
 
 /**
  * Retrieves the card number from the form.
