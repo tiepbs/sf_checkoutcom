f67b9d814e0269595afd764f606beac2d465ddef
david-fiaty-cko <david.fiaty@checkout.com>
2020-01-29 12:16:33 +0000

    Updated the submit button state handling

--- Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/checkoutcom.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/checkoutcom.js
@@ -3,64 +3,66 @@
 /**
  * jQuery Ajax helpers on DOM ready.
  */
-document.addEventListener('DOMContentLoaded', function () {
-    
-    checkPaymentButtonState();
+document.addEventListener('DOMContentLoaded', function() {
+
+	checkPaymentButtonState();
 
 }, false);
 
 function checkPaymentButtonState() {
+    $('button.submit-payment').hide();
     $('#ckoSubmitPayment').show();
 }
 
 /**
  * Retrieves the card number from the form.
  */
-function getCardData(elt, dataUrl)
-{
-    // Get the selected card UUID
-    var cardUUID = elt.options[elt.selectedIndex].value;
-    if (cardUUID.length !== 0) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', dataUrl);
-        xhr.onload = function () {
-            if (this.status == 200) {
-                var cards = JSON.parse(this.response.replace(/&quot;/g,'"'));
-                
-                // Find the corresponding card
-                for (var i = 0; i < cards.length; i++) {
-                    if (cards[i].cardId == cardUUID) {
-                        setFields({
-                            cardId: cards[i].cardId,
-                            cardNumber: cards[i].cardNumber,
-                            cardType: cards[i].cardType,
-                            cardHolder: cards[i].cardHolder,
-                            cardType: cards[i].cardType,
-                            expiryMonth: cards[i].expiryMonth,
-                            expiryYear: cards[i].expiryYear,
-                        });
+function getCardData(elt, dataUrl) {
+	// Get the selected card UUID
+	var cardUUID = elt.options[elt.selectedIndex].value;
+	if (cardUUID.length !== 0) {
+		const xhr = new XMLHttpRequest();
+		xhr.open('POST', dataUrl);
+		xhr.onload = function() {
+			if (this.status == 200) {
+				var cards = JSON.parse(this.response.replace(/&quot;/g, '"'));
 
-                        // Break the loop
-                        return;
-                    }
-                }
-            }
-        };
-        xhr.send();
-    }
+				// Find the corresponding card
+				for (var i = 0; i < cards.length; i++) {
+					if (cards[i].cardId == cardUUID) {
+						setFields({
+							cardId : cards[i].cardId,
+							cardNumber : cards[i].cardNumber,
+							cardType : cards[i].cardType,
+							cardHolder : cards[i].cardHolder,
+							cardType : cards[i].cardType,
+							expiryMonth : cards[i].expiryMonth,
+							expiryYear : cards[i].expiryYear,
+						});
+
+						// Break the loop
+						return;
+					}
+				}
+			}
+		};
+		xhr.send();
+	}
 }
 
 /**
  * Sets the card form fields from user card data.
  */
-function setFields(data)
-{
-    var $creditCard = $('[data-method="CHECKOUTCOM_CARD"]');
-    $creditCard.find('input[name$="_cardPaymentForm_owner"]').val(data.cardHolder).trigger('change');
-    $creditCard.find('input[name$="_cardPaymentForm_number"]').val(data.cardNumber).trigger('change');
-    // enable card data formating
-    setSchema('#dwfrm_cardPaymentForm_number');
-    $creditCard.find('[name$="_month"]').val(data.expiryMonth).trigger('change');
-    $creditCard.find('[name$="_year"]').val(data.expiryYear).trigger('change');
-    $creditCard.find('input[name$="_cvn"]').val('').trigger('change');
+function setFields(data) {
+	var $creditCard = $('[data-method="CHECKOUTCOM_CARD"]');
+	$creditCard.find('input[name$="_cardPaymentForm_owner"]').val(
+			data.cardHolder).trigger('change');
+	$creditCard.find('input[name$="_cardPaymentForm_number"]').val(
+			data.cardNumber).trigger('change');
+	// enable card data formating
+	setSchema('#dwfrm_cardPaymentForm_number');
+	$creditCard.find('[name$="_month"]').val(data.expiryMonth)
+			.trigger('change');
+	$creditCard.find('[name$="_year"]').val(data.expiryYear).trigger('change');
+	$creditCard.find('input[name$="_cvn"]').val('').trigger('change');
 }
