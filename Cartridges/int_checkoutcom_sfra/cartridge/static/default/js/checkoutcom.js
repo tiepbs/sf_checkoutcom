b85d9e52ae72c4afee90ec267a67621ec92a4810
david-fiaty-cko <david.fiaty@checkout.com>
2020-02-07 17:16:25 +0000

    Addded email autofill in billing form for registered customers

--- Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/checkoutcom.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/checkoutcom.js
@@ -9,11 +9,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	// Handle payment tabs state
 	initTabs();
-	
+
 	// Tooltips
 	initTooltips();
-
+	
+	// Customer email
+	prepareCustomerEmail();
+		
 }, false);
+
+function prepareCustomerEmail() {
+	$('input#email').val(
+		$('#ckoCustomerEmail').val()
+	);
+}
 
 function initTooltips() {
 	$('#cko-card-content .icon').off('click').on('click', function() {
@@ -24,7 +33,7 @@ function initTooltips() {
 function initButtons() {
 	// Hide the global form button
 	$('button.submit-payment').hide();
-	
+
 	// Show the custom form button
 	$('#ckoSubmitPayment').show();
 }
@@ -35,19 +44,20 @@ function initTabs() {
 
 	// Handle the click navigation
 	var allTabs = $('.payment-options a.nav-link');
-	allTabs.off('click').on('click', function () {
-		// Hide all tabs contents
-		$('.ckoPaymentOptions .tab-pane').hide();
+	allTabs.off('click').on(
+			'click',
+			function() {
+				// Hide all tabs contents
+				$('.ckoPaymentOptions .tab-pane').hide();
 
-		// Show the clicked tab content
-		$($(this).attr('href')).show();
+				// Show the clicked tab content
+				$($(this).attr('href')).show();
 
-		// Set the selected option id
-		$('#selectedPaymentOption').val(
-			$(this).closest('li').data('method-id')
-		);
-	});
-	
+				// Set the selected option id
+				$('#selectedPaymentOption').val(
+						$(this).closest('li').data('method-id'));
+			});
+
 	// Show the first active
 	$('.card-tab').trigger('click');
 }
