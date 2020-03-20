393bea6bc944bee166d0e1c285e642d12e24f947
david-fiaty-cko <david.fiaty@checkout.com>
2020-03-20 00:54:09 +0000

    Improved the billing and shipping validation code

--- Cartridges/int_checkoutcom_sfra/cartridge/controllers/CheckoutServices.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/controllers/CheckoutServices.js
@@ -84,15 +84,131 @@ server.replace('SubmitPayment', server.middleware.https, function (req, res, nex
 
     var billingForm = server.forms.getForm('billing');
 
-    res.json({
-        renderedPaymentInstruments: '',
-        customer: accountModel,
-        order: basketModel,
-        form: billingForm,
-        error: false
-    });    
-    
+    this.on('route:BeforeComplete', function (req, res) {
+        var BasketMgr = require('dw/order/BasketMgr');
+        var HookMgr = require('dw/system/HookMgr');
+        var PaymentMgr = require('dw/order/PaymentMgr');
+        var PaymentInstrument = require('dw/order/PaymentInstrument');
+        var Transaction = require('dw/system/Transaction');
+        var AccountModel = require('*/cartridge/models/account');
+        var OrderModel = require('*/cartridge/models/order');
+        var URLUtils = require('dw/web/URLUtils');
+        var Locale = require('dw/util/Locale');
+        var basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
+        var hooksHelper = require('*/cartridge/scripts/helpers/hooks');
+        var validationHelpers = require('*/cartridge/scripts/helpers/basketValidationHelpers');
 
+        var currentBasket = BasketMgr.getCurrentBasket();
+
+        var billingData = res.getViewData();
+
+        if (!currentBasket) {
+            delete billingData.paymentInformation;
+
+            res.json({
+                error: true,
+                cartError: true,
+                fieldErrors: [],
+                serverErrors: [],
+                redirectUrl: URLUtils.url('Cart-Show').toString()
+            });
+            return;
+        }
+
+        var validatedProducts = validationHelpers.validateProducts(currentBasket);
+        if (validatedProducts.error) {
+            delete billingData.paymentInformation;
+
+            res.json({
+                error: true,
+                cartError: true,
+                fieldErrors: [],
+                serverErrors: [],
+                redirectUrl: URLUtils.url('Cart-Show').toString()
+            });
+            return;
+        }
+
+        var billingAddress = currentBasket.billingAddress;
+        var billingForm = server.forms.getForm('billing');
+        billingForm.creditCardFields.cardNumber.htmlValue = '';
+        billingForm.creditCardFields.securityCode.htmlValue = '';
+
+        Transaction.wrap(function () {
+            if (!billingAddress) {
+                billingAddress = currentBasket.createBillingAddress();
+            }
+
+            billingAddress.setFirstName(billingData.address.firstName.value);
+            billingAddress.setLastName(billingData.address.lastName.value);
+            billingAddress.setAddress1(billingData.address.address1.value);
+            billingAddress.setAddress2(billingData.address.address2.value);
+            billingAddress.setCity(billingData.address.city.value);
+            billingAddress.setPostalCode(billingData.address.postalCode.value);
+            if (Object.prototype.hasOwnProperty.call(billingData.address, 'stateCode')) {
+                billingAddress.setStateCode(billingData.address.stateCode.value);
+            }
+            billingAddress.setCountryCode(billingData.address.countryCode.value);
+
+            if (billingData.storedPaymentUUID) {
+                billingAddress.setPhone(req.currentCustomer.profile.phone);
+                currentBasket.setCustomerEmail(req.currentCustomer.profile.email);
+            } else {
+                billingAddress.setPhone(billingData.phone.value);
+                currentBasket.setCustomerEmail(billingData.email.value);
+            }
+        });
+
+        // Calculate the basket
+        Transaction.wrap(function () {
+            basketCalculationHelpers.calculateTotals(currentBasket);
+        });
+
+        // Re-calculate the payments.
+        var calculatedPaymentTransaction = COHelpers.calculatePaymentTransaction(
+            currentBasket
+        );
+
+        if (calculatedPaymentTransaction.error) {
+            res.json({
+                form: paymentForm,
+                fieldErrors: [],
+                serverErrors: [Resource.msg('error.technical', 'checkout', null)],
+                error: true
+            });
+            return;
+        }
+
+        var usingMultiShipping = req.session.privacyCache.get('usingMultiShipping');
+        if (usingMultiShipping === true && currentBasket.shipments.length < 2) {
+            req.session.privacyCache.set('usingMultiShipping', false);
+            usingMultiShipping = false;
+        }
+
+        var currentLocale = Locale.getLocale(req.locale.id);
+
+        var basketModel = new OrderModel(
+            currentBasket,
+            { usingMultiShipping: usingMultiShipping, countryCode: currentLocale.country, containerView: 'basket' }
+        );
+
+        var accountModel = new AccountModel(req.currentCustomer);
+        var renderedStoredPaymentInstrument = COHelpers.getRenderedPaymentInstruments(
+            req,
+            accountModel
+        );
+
+        delete billingData.paymentInformation;
+
+        res.json({
+            renderedPaymentInstruments: renderedStoredPaymentInstrument,
+            customer: accountModel,
+            order: basketModel,
+            form: billingForm,
+            error: false
+        });    
+    });
+  
    return next();
 });
 
