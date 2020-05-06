97c8f5fda0b20382b4bcf588b7398c001f3b7454
david-fiaty-cko <david.fiaty@checkout.com>
2020-05-06 01:41:38 +0100

    Added the oxxo field validation code

---
+++ Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/apm/oxxo.js
@@ -0,0 +1,16 @@
+function oxxoFieldsValidation() {
+    // Errors count
+    var errors = 0;
+
+    // Field 1 validation
+    var field1 = $('#oxxo_identification');
+    if (field1.val() == '') {
+        $('#oxxo_pay_box .invalid-field-message').text(
+            window.ckoLang.apmFieldInvalid
+        );
+        field1.addClass('is-invalid');
+        errors++;
+    }
+
+    return errors;
+}
