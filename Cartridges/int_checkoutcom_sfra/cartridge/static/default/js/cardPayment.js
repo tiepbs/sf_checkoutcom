3bfafd022df501079b2d41b986dba870c147d7f9
david-fiaty-cko <david.fiaty@checkout.com>
2020-05-09 04:18:05 +0100

    Imporved the selected saved card code

--- Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/cardPayment.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/cardPayment.js
@@ -2,6 +2,23 @@
 
 // Set events on page loaded
 document.addEventListener('DOMContentLoaded', function () { 
-    
+    initSavedCardSelection();
 });
 
+function initSavedCardSelection() {
+    // Set the selected card uuid
+    $('.saved-payment-instrument').off('click touch').on('click touch', function (e) {
+        var self = $(this);
+        $('input[name="selectedCardUuid"]').val(
+            self.data('uuid')
+        );
+    });
+
+    // Set the card cvv event
+    $('.saved-payment-instrument').on('change', function (e) {
+        var self = $(this);
+        $('input[name="selectedCardCvv"]').val(
+            self.find('input.saved-payment-security-code').val()
+        );
+    });
+}
