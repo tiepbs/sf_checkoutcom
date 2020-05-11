844aa64e21006c5167cb9595528c76d667236c4e
david-fiaty-cko <david.fiaty@checkout.com>
2020-05-11 23:44:46 +0100

    Added code improvements

--- Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/cardPayment.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/cardPayment.js
@@ -2,28 +2,37 @@
 
 // Set events on page loaded
 document.addEventListener('DOMContentLoaded', function () { 
-    // Sasved card selection
+    // Saved card selection
     initSavedCardSelection();
 });
 
 function initSavedCardSelection() {
-    // Set the selected card uuid
-    $('.saved-payment-instrument').on('click touch', function (e) {
-        var self = $(this);
+    // Is card payment
+    var condition1 = $('input[name="dwfrm_billing_paymentMethod"]').val() == 'CHECKOUTCOM_CARD';
 
+    // Saved cards invisible
+    var condition2 = $('.user-payment-instruments').hasClass('checkout-hidden');
+
+    // Validation event
+    if (condition1 && !condition2) {
         // Set the selected card uuid
-        $('input[name="dwfrm_billing_creditCardFields_selectedCardUuid"]').val(
-            self.data('uuid')
-        );
-    });
+        $('.saved-payment-instrument').on('click touch', function (e) {
+            var self = $(this);
 
-    // Set the card cvv event
-    $('.saved-payment-instrument').on('change', function (e) {
-        var self = $(this);
-        $('input[name="dwfrm_billing_creditCardFields_selectedCardCvv"]').val(
-            self.find('input.saved-payment-security-code').val()
-        );
-    });
+            // Set the selected card uuid
+            $('input[name="dwfrm_billing_creditCardFields_selectedCardUuid"]').val(
+                self.data('uuid')
+            );
+        });
+
+        // Set the card cvv event
+        $('.saved-payment-instrument').on('change', function (e) {
+            var self = $(this);
+            $('input[name="dwfrm_billing_creditCardFields_selectedCardCvv"]').val(
+                self.find('input.saved-payment-security-code').val()
+            );
+        });
+    }
 }
 
 function initCheckoutcomCardValidation() {
