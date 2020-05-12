f953708bde21e0bddb0f383da471483561e1d6da
david-fiaty-cko <david.fiaty@checkout.com>
2020-05-12 23:39:49 +0100

    Added complementary logic for saved cards handling

---
+++ Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/savedCardPayment.js
@@ -0,0 +1,81 @@
+function initCheckoutcomCardValidation() {
+    // Is card payment
+    var condition1 = $('input[name="dwfrm_billing_paymentMethod"]').val() == 'CHECKOUTCOM_CARD';
+
+    // Saved cards invisible
+    var condition2 = $('.user-payment-instruments').hasClass('checkout-hidden');
+
+    // Run the form validation
+    if (condition1 && !condition2) {
+        savedCardFormValidation();
+        savedCardSelection();
+        $('.saved-payment-instrument').first().addClass('selected-payment');
+    }
+}
+
+function savedCardFormValidation() {
+    alert('savedCardFormValidation');
+    $('button.submit-payment').off('click touch').one('click touch', function (e) {
+        // Reset the form error messages
+        resetFormErrors();
+
+        // Prepare some variables
+        var savedCard = $('.saved-payment-instrument');
+        var buttonEvent = e;
+
+        // Implement the event
+        savedCard.each(function(i) {
+            // Prepare the variables
+            var self = $(this);
+            var cvvField = self.find('input.saved-payment-security-code');
+
+            // The saved card is selected
+            var condition3 = self.hasClass('selected-payment');
+
+            // Field is empty
+            var condition4 = cvvField.val() == '';
+
+            // Field is numeric
+            var condition5 = cvvField.val()%1 == 0;
+
+            // Field validation
+            if (condition3 && (condition4 || !condition5)) {
+                // Prevent the default button click behaviour
+                buttonEvent.preventDefault();
+                buttonEvent.stopImmediatePropagation();
+        
+                // Show the CVV error
+                self.find('.invalid-feedback').show();
+            }
+        });
+    }); 
+}
+
+function savedCardSelection() {
+    // Is card payment
+    var condition2 = $('input[name="dwfrm_billing_paymentMethod"]').val() == 'CHECKOUTCOM_CARD';
+
+    // Saved cards invisible
+    var condition3 = $('.user-payment-instruments').hasClass('checkout-hidden');
+
+    // Validation event
+    if (condition2 && !condition3) {
+        // Set the selected card uuid
+        $('.saved-payment-instrument').off('click touch').one('click touch', function (e) {
+            var self = $(this);
+
+            // Set the selected card uuid
+            $('input[name="dwfrm_billing_savedCardForm_selectedCardUuid"]').val(
+                self.data('uuid')
+            );
+        });
+
+        // Set the card cvv event
+        $('.saved-payment-instrument').off('click touch').one('click touch', function (e) {
+            var self = $(this);
+            $('input[name="dwfrm_billing_savedCardForm_selectedCardCvv"]').val(
+                self.find('input.saved-payment-security-code').val()
+            );
+        });
+    }
+}
