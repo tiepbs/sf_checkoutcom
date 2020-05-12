1c6b4e5c1cd49a3a2b8135007db917f0b03cb023
david-fiaty-cko <david.fiaty@checkout.com>
2020-05-12 13:31:12 +0100

    Updated the APM redirection

--- Cartridges/int_checkoutcom_sfra/cartridge/scripts/helpers/apmHelper.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/scripts/helpers/apmHelper.js
@@ -90,6 +90,7 @@ var apmHelper = {
 
         // Add redirect to sepa source reqeust
         if (!result.error && type == 'Sepa') {
+            result.error = false;
             session.privacy.redirectUrl = URLUtils.url('CKOSepa-Mandate').value;
             result.redirectUrl = URLUtils.url('CKOSepa-Mandate').value;
             session.privacy.sepaResponseId = gatewayResponse.id;
@@ -97,6 +98,7 @@ var apmHelper = {
         
         // Add redirect URL to session if exists
         if (!result.error && gatewayResponse.hasOwnProperty('_links')) {
+            result.error = false;
             var gatewayLinks = gatewayResponse._links;
             session.privacy.redirectUrl = gatewayLinks.redirect.href
             result.redirectUrl = gatewayLinks.redirect.href;
