e58df303c532cb4b8d38ac2760e3d90ea06f57b8
david-fiaty-cko <david.fiaty@checkout.com>
2020-06-03 01:09:55 +0100

    Added an account model override

---
+++ Cartridges/int_checkoutcom_sfra/cartridge/models/account.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var URLUtils = require('dw/web/URLUtils');
+var base = module.superModule;
+
+/**
+ * Creates a plain object that contains payment instrument information
+ * @param {Object} userPaymentInstruments - current customer's paymentInstruments
+ * @returns {Object} object that contains info about the current customer's payment instruments
+ */
+function getCustomerPaymentInstruments(userPaymentInstruments) {
+    var paymentInstruments;
+
+    paymentInstruments = userPaymentInstruments.map(function (paymentInstrument) {
+        var result = {
+            creditCardHolder: paymentInstrument.creditCardHolder,
+            maskedCreditCardNumber: paymentInstrument.maskedCreditCardNumber,
+            creditCardType: paymentInstrument.creditCardType,
+            creditCardExpirationMonth: paymentInstrument.creditCardExpirationMonth,
+            creditCardExpirationYear: paymentInstrument.creditCardExpirationYear,
+            UUID: paymentInstrument.UUID
+        };
+
+        result.cardTypeImage = {
+            src: URLUtils.staticURL('/images/' +
+                paymentInstrument.creditCardType.toLowerCase().replace(/\s/g, '') +
+                '-dark.svg'),
+            alt: paymentInstrument.creditCardType
+        };
+
+        return result;
+    });
+
+    return paymentInstruments;
+}
+
+/**
+ * Account class that represents the current customer's profile dashboard
+ * @param {Object} currentCustomer - Current customer
+ * @param {Object} addressModel - The current customer's preferred address
+ * @param {Object} orderModel - The current customer's order history
+ * @constructor
+ */
+function account(currentCustomer, addressModel, orderModel) {
+    this.profile = getProfile(currentCustomer.profile);
+    this.addresses = getAddresses(currentCustomer.addressBook);
+    this.preferredAddress = addressModel || getPreferredAddress(currentCustomer.addressBook);
+    this.orderHistory = orderModel;
+    this.payment = getPayment(currentCustomer.wallet);
+    this.registeredUser = currentCustomer.raw.authenticated && currentCustomer.raw.registered;
+    this.isExternallyAuthenticated = currentCustomer.raw.externallyAuthenticated;
+    this.customerPaymentInstruments = currentCustomer.wallet
+        && currentCustomer.wallet.paymentInstruments
+        ? getCustomerPaymentInstruments(currentCustomer.wallet.paymentInstruments)
+        : null;
+}
+
+getCustomerPaymentInstruments.prototype = Object.create(base.prototype);
+account.prototype = Object.create(base.prototype);
+
+module.exports.getCustomerPaymentInstruments = getCustomerPaymentInstruments;
+module.exports.account = account;
