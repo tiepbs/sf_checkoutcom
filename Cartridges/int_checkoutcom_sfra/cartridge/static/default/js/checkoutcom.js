c1e982efefba7e29dd2d27ebd019a6f2172dbcab
david-fiaty-cko <david.fiaty@checkout.com>
2020-06-04 11:34:48 +0100

    Improved the saved cards display

--- Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/checkoutcom.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/checkoutcom.js
@@ -21,6 +21,9 @@ function initTabs() {
 			$('.tab-pane').removeClass('active');
 			$('a.nav-link').removeClass('active');
 
+			// Handle the saved cards display
+			handleSavedCardsDisplay();
+
 			// Get the target id
 			var targetId = $(this).attr('href');
 
@@ -44,6 +47,14 @@ function initTabs() {
 
 	// Show the first active
 	$('.payment-options li.nav-item').first().find('a.nav-link').trigger('click');
+}
+
+function handleSavedCardsDisplay() {
+	var savedCards = $('.row.saved-payment-instrument').length;
+	if (savedCards == 0) {
+		$('.saved-card-tab').closest('li').hide();
+		$('.tab-pane.saved-card-content').hide();
+	}
 }
 
 function initFormValidation() {
