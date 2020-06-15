59fc85745a967152efb03f7ddaeef663092460d8
david-fiaty-cko <david.fiaty@checkout.com>
2020-06-15 22:48:24 +0100

    Updated the Paypal currency filter list

--- Cartridges/int_checkoutcom_sfra/cartridge/scripts/config/ckoApmFilterConfig.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/scripts/config/ckoApmFilterConfig.js
@@ -70,8 +70,8 @@ var ckoApmFilterConfig = {
     	currencies	: ["USD", "CNY"]
     },
     paypal: {
-    	countries	: "*",
-    	currencies	: ["*"]
+    	countries	: ["*"],
+    	currencies	: ["AUD", "BRL", "CAD", "CZK", "DKK", "EUR", "HKD", "HUF", "INR", "ILS", "JPY", "MYR", "MXN", "TWD", "NZD", "NOK", "PHP", "PLN", "GBP", "RUB", "SGD", "SEK", "CHF", "THB", "USD"]
     }
 }
 
