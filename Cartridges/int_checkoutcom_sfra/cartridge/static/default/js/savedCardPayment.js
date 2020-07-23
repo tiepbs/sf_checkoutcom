300d59e5e6bf5a7b00894296bb4e9907b56192bb
david-fiaty-cko <david.fiaty@checkout.com>
2020-07-24 00:29:49 +0100

    Moved the save card methods

--- Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/savedCardPayment.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/static/default/js/savedCardPayment.js
@@ -1,50 +1,6 @@
 'use strict';
 
 /**
- * Validate the save card form
- */
-function savedCardFormValidation() {
-  // Enable the saved card selection
-  savedCardSelection();
-
-  // Submit event
-  $('button.submit-payment').off('click touch').one('click touch', function(e) {
-    // Reset the form error messages
-    resetFormErrors();
-
-    // Prepare some variables
-    var savedCard = $('.saved-payment-instrument');
-    var buttonEvent = e;
-
-    // Implement the event
-    savedCard.each(function(i) {
-      // Prepare the variables
-      var self = $(this);
-      var cvvField = self.find('input.saved-payment-security-code');
-
-      // The saved card is selected
-      var condition1 = self.hasClass('selected-payment');
-
-      // Field is empty
-      var condition2 = cvvField.val() == '';
-
-      // Field is numeric
-      var condition3 = cvvField.val() % 1 == 0;
-
-      // Field validation
-      if (condition1 && (condition2 || !condition3)) {
-        // Prevent the default button click behaviour
-        buttonEvent.preventDefault();
-        buttonEvent.stopImmediatePropagation();
-
-        // Show the CVV error
-        self.find('.invalid-feedback').show();
-      }
-    });
-  });
-}
-
-/**
  * Handle the saved card selection
  */
 function savedCardSelection() {
