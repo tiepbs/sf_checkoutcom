77d94aef15d4fa872eca70a39f3c55a54e85f238
david-fiaty-cko <david.fiaty@checkout.com>
2020-08-27 16:27:36 +0100

    Updated apple pay hooks

--- Cartridges/int_checkoutcom_sfra/cartridge/scripts/hooks/payment/processor/CHECKOUTCOM_APPLE_PAY.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/scripts/hooks/payment/processor/CHECKOUTCOM_APPLE_PAY.js
@@ -1,61 +1,51 @@
+var Status = require('dw/system/Status');
+var PaymentInstrument = require('dw/order/PaymentInstrument');
+var Logger = require('dw/system/Logger');
+
+
 'use strict';
 
 /** Utility **/
 var ckoHelper = require('~/cartridge/scripts/helpers/ckoHelper');
 var applePayHelper = require('~/cartridge/scripts/helpers/applePayHelper');
 
-/**
- * Verifies that the payment data is valid.
- * @param {Object} basket The basket instance
- * @param {Object} billingData The billing data
- * @param {string} processorId The processor id
- * @param {Object} req The HTTP request data
- * @returns {Object} The form validation result
- */
-function Handle(basket, billingData, processorId, req) {
-    var fieldErrors = {};
-    var serverErrors = [];
+exports.authorizeOrderPayment = function (order) {
 
-    return {
-        fieldErrors: fieldErrors,
-        serverErrors: serverErrors,
-        error: false,
-    };
-}
-
-/**
- * Authorizes a payment.
- * @param {Object} orderNumber The order number
- * @param {Object} billingForm The billing data
- * @param {string} processorId The processor id
- * @param {Object} req The HTTP request data
- * @returns {Object} The payment result
- */
-function Authorize(orderNumber, billingForm, processorId, req) {
-    var serverErrors = [];
-    var fieldErrors = {};
 
     // Payment request
+    /*
     var result = applePayHelper.handleRequest(
         billingForm.applePayForm.ckoApplePayData.htmlValue,
         processorId,
         orderNumber
     );
+     */
 
-    // Handle errors
-    if (result.error) {
-        serverErrors.push(
-            ckoHelper.getPaymentFailureMessage()
-        );
+
+
+
+    order.addNote('Payment Authorization Warning!', 'This is a dummy ' +
+      'authorizeOrderPayment hook implementation. Please disable it to use ' +
+      'the built-in PSP API, or implement the necessary calls to the ' +
+      'Payment Provider for authorization.');
+    var paymentInstruments = order.getPaymentInstruments(
+        PaymentInstrument.METHOD_DW_APPLE_PAY).toArray();
+    if (!paymentInstruments.length) {
+        Logger.error('Unable to find Apple Pay payment instrument for order.');
+        return null;
     }
+    var paymentInstrument = paymentInstruments[0];
+    var paymentTransaction = paymentInstrument.getPaymentTransaction();
+    paymentTransaction.setTransactionID('DUMMY-APPLEPAY-PSP-TRANSACTION-ID');
+    return new Status(Status.OK);
+};
 
-    return {
-        fieldErrors: fieldErrors,
-        serverErrors: serverErrors,
-        error: result.error,
-        redirectUrl: result.redirectUrl,
-    };
-}
 
-exports.Handle = Handle;
-exports.Authorize = Authorize;
+exports.getRequest = function (basket, req) {
+
+    var logger = require('dw/system/Logger').getLogger('ckodebug');
+    logger.debug('AP request 1 {0}', JSON.stringify(req));
+    
+
+    session.custom.applepaysession = 'yes';   // eslint-disable-line
+};
