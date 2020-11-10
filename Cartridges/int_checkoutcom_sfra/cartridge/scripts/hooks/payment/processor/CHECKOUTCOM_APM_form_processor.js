e46f57c36f5c582e480a2091f04ee195f52a05fe
michael-ugborioko-cko <michael.ugborikoko@checkout.com>
2020-11-10 12:18:37 +0000

    Merged Card Payment and APM forms

--- Cartridges/int_checkoutcom_sfra/cartridge/scripts/hooks/payment/processor/CHECKOUTCOM_APM_form_processor.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/scripts/hooks/payment/processor/CHECKOUTCOM_APM_form_processor.js
@@ -6,24 +6,44 @@
  * @param {Object} viewFormData Object contains billing form data
  * @returns {Object} An object that has error information or payment information
  */
-function processForm(paymentForm, viewFormData) {
+function processForm(req, paymentForm, viewFormData) {
     var viewData = viewFormData;
     var ckoSelectedApm = paymentForm.apmForm ? paymentForm.apmForm.ckoSelectedApm.htmlValue : null;
     var error = true;
 
     if (ckoSelectedApm) {
+        error = false;
         viewData.paymentMethod = {
             value: paymentForm.paymentMethod.htmlValue,
-            htmlName: paymentForm.paymentMethod.htmlValue,
+            htmlName: paymentForm.paymentMethod.htmlName,
         };
 
-        viewData.paymentInformation = {
-            ckoApm: {
-                value: ckoSelectedApm.htmlValue,
-                htmlName: ckoSelectedApm.htmlValue,
-                data: paymentForm.apmForm,
-            },
-        };
+        var apmForm = ckoSelectedApm + 'Form';
+        var apmData = paymentForm[apmForm];
+        viewData.paymentInformation = {};
+
+        if (apmData) {
+
+            viewData.paymentInformation['type'] = {
+                value: ckoSelectedApm,
+                htmlName: apmData.htmlName
+            };
+            Object.keys(apmData).forEach(function(key) {
+                var type = typeof apmData[key];
+                if (type == 'object' && apmData[key] != null) {
+                    viewData.paymentInformation[key] = {
+                        value: apmData[key].htmlValue,
+                        htmlName: apmData[key].htmlName
+                    };
+                }
+            });
+        } else {
+            viewData.paymentInformation['type'] = {
+                value: ckoSelectedApm,
+                htmlName: paymentForm.apmForm.htmlName
+            };
+        }
+
     }
 
     return {
