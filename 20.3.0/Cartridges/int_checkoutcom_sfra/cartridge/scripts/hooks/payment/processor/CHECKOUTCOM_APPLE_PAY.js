3173a886de64776d455ffe9b473051fde31e5ded
michael-ugborioko-cko <michael.ugborikoko@checkout.com>
2020-11-27 22:01:51 +0000

    Apple pay implementation

--- 20.3.0/Cartridges/int_checkoutcom_sfra/cartridge/scripts/hooks/payment/processor/CHECKOUTCOM_APPLE_PAY.js
+++ 20.3.0/Cartridges/int_checkoutcom_sfra/cartridge/scripts/hooks/payment/processor/CHECKOUTCOM_APPLE_PAY.js
@@ -1,31 +1,54 @@
 'use strict';
 
+// API Includes
 var Status = require('dw/system/Status');
+var PaymentInstrument = require('dw/order/PaymentInstrument');
+var Logger = require('dw/system/Logger');
 
 /** Utility **/
 var applePayHelper = require('~/cartridge/scripts/helpers/applePayHelper');
 
 exports.authorizeOrderPayment = function (order, event) {
+
     var condition = Object.prototype.hasOwnProperty.call(event, 'isTrusted')
-    && event.isTrusted === true
+    && event.isTrusted === true 
     && order;
 
     if (condition) {
-        // Payment request
-        var result = applePayHelper.handleRequest(
-            event.payment.token.paymentData,
-            'CHECKOUTCOM_APPLE_PAY',
-            order.orderNo
-        );
+        try {
+            order.addNote('Payment Authorization Request:', 'Checkout.com Applepay');
+            var paymentInstruments = order.getPaymentInstruments(
+                PaymentInstrument.METHOD_DW_APPLE_PAY).toArray();
+            if (!paymentInstruments.length) {
+                Logger.error('Unable to find Apple Pay payment instrument for order.');
+                return null;
+            }
+            var paymentInstrument = paymentInstruments[0];
+            var PaymentMgr = require('dw/order/PaymentMgr');
+            var paymentProcessor = PaymentMgr.getPaymentMethod(paymentInstrument.getPaymentMethod()).getPaymentProcessor();
+            var paymentTransaction = paymentInstrument.getPaymentTransaction();
+            paymentTransaction.setTransactionID(order.orderNo);
+            paymentTransaction.setPaymentProcessor(paymentProcessor);
 
-        if (!result.error) {
+            // Payment request
+            var result = applePayHelper.handleRequest(
+                event.payment.token.paymentData,
+                paymentProcessor.ID,
+                order.orderNo
+            );
+
+            if (result.error) {
+                throw new Error({message: 'Payment Authorization error'});
+            }
+
+            order.addNote('Payment Authorization Request:', 'Payment Authorization successful');
             return new Status(Status.OK);
+
+        } catch (e) {
+            order.addNote('Payment Authorization Request:', e.message);
+            return new Status(Status.ERROR);
         }
     }
 
     return new Status(Status.ERROR);
-};
-
-exports.getRequest = function (basket, req) {
-    session.custom.applepaysession = 'yes';  // eslint-disable-line
 };
