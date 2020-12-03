4c61d9540951e1a1b25310151bcd4665d67a869e
Anghelus Alin <alin.anghelus@endava.com>
2020-12-03 14:08:49 +0200

    fixed code issues
    - fixed wrong modified code

--- 20.3.0/Cartridges/int_checkoutcom_sfra/cartridge/scripts/hooks/payment/processor/CHECKOUTCOM_CARD.js
+++ 20.3.0/Cartridges/int_checkoutcom_sfra/cartridge/scripts/hooks/payment/processor/CHECKOUTCOM_CARD.js
@@ -174,7 +174,7 @@ function Handle(basket, paymentInformation, paymentMethodID, req) {
         paymentInstrument.setCreditCardExpirationYear(expirationYear);
 
         // Create card token if save card is true
-        if (paymentInformation.saveCard.value) {
+        if (paymentInformation.saveCard.value  && !paymentInformation.storedPaymentUUID) {
             paymentInstrument.setCreditCardToken(
                 paymentInformation.creditCardToken
                     ? paymentInformation.creditCardToken
@@ -184,7 +184,7 @@ function Handle(basket, paymentInformation, paymentMethodID, req) {
         paymentInstrument.custom.ckoPaymentData = JSON.stringify({
             'securityCode': cardSecurityCode,
             'storedPaymentUUID': paymentInformation.storedPaymentUUID,
-            'saveCard': paymentInformation.saveCard.value,
+            'saveCard': paymentInformation.creditCardToken ? true : false,
             'customerNo': req.currentCustomer.raw.registered ? req.currentCustomer.profile.customerNo : null ,
             'madaCard': madaCard
         });
