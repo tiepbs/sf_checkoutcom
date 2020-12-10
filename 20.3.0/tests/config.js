5c025a275f6d3e98fe43c6eef27073e637711db7
michael-ugborioko-cko <michael.ugborikoko@checkout.com>
2020-12-10 09:00:49 +0000

    remove release 20.2.0

--- 20.3.0/tests/config.js
+++
@@ -1,6 +0,0 @@
-var config = {
-  sgUrl: "https://checkout01-tech-prtnr-eu02-dw.demandware.net/on/demandware.store/Sites-SiteGenesisGlobal-Site/en_GB/",
-  sfraUrl: "https://checkout01-tech-prtnr-eu02-dw.demandware.net/on/demandware.store/Sites-RefArchGlobal-Site/en_GB/"
-}
-
-module.exports = config;
